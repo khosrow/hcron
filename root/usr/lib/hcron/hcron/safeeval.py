e5c1f828b8bdffe5b1b3c60f374679594b390b88
John Marshall <John.Marshall@ec.gc.ca>
2009-10-20 14:54:42 -0400

    Update dates (copyright and version)

--- root/usr/lib/hcron/hcron/safeeval.py
+++ root/usr/lib/hcron/hcron/safeeval.py
@@ -4,7 +4,7 @@
 
 # GPL--start
 # This file is part of hcron
-# Copyright (C) 2008 Environment/Environnement Canada
+# Copyright (C) 2008, 2009 Environment/Environnement Canada
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
