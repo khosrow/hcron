ac95625a18afc3eb1de1ce28f6697f1940dd9247
John Marshall <John.Marshall@ec.gc.ca>
2010-04-29 21:55:37 +0000

    snapshot: hcron tree support

---
+++ static/usr/lib/hcron/hcron/hcrontree.py
@@ -0,0 +1,194 @@
+#! /usr/bin/env python
+#
+# hcron/hcrontree.py
+
+# GPL--start
+# This file is part of hcron
+# Copyright (C) 2010 Environment/Environnement Canada
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 2
+# of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# GPL--end
+
+"""Hcron tree support.
+"""
+
+# system imports
+import os
+import os.path
+import shutil
+import tarfile
+
+#
+from constants import *
+import globals
+from library import copyfile
+from logging import *
+
+class HcronTreeCache:
+    """Interface to packaged hcron tree file containing members as:
+        includes/...
+        events/...
+    """
+
+    #def __init__(self, path, ignore_match_fn=None):
+    def __init__(self, username, ignore_match_fn=None):
+        def false_match(*args):
+            return False
+
+        self.username = username
+        self.ignore_match_fn = ignore_match_fn or false_match
+        self.path = get_hcron_tree_filename(username, HOST_NAME)
+        self.cache = {}
+        self.load()
+
+    def load(self):
+        rejected = {}
+        link_cache = {}
+        cache = {}
+        f = tarfile.open(self.path)
+
+        for m in f.getmembers():
+            if m.name.startswith("includes/") or m.name.startswith("events/"):
+                if os.path.dirname(m.name) in rejected:
+                    rejected[m.name] = None
+                    continue
+                elif self.ignore_match_fn(os.path.basename(m.name)):
+                    rejected[m.name] = None
+                    continue
+                else:
+                    if m.issym():
+                        link_cache[m.name] = self.resolve_symlink(m.name, m.linkname)
+                    elif m.isfile():
+                        cache[m.name] = f.extractfile(m).read()
+
+        f.close()
+
+        # resolve for symlinks
+        for name, linkname in link_cache.items():
+            for _ in xrange(10):
+                if linkname in cache:
+                    cache[name] = cache[linkname]
+                    break
+                elif linkname in link_cache:
+                    linkname = resolve_symlink(linkname, link_cache[linkname])
+                else:
+                    # not found; drop
+                    break
+
+        self.cache = cache
+
+    def resolve_symlink(self, name, linkname):
+        if linkname.startswith("/"):
+            return None
+        else:
+            return os.path.normpath(os.path.dirname(name)+"/"+linkname)
+
+    def get_event_contents(self, name):
+        if name.startswith("/"):
+            return self.get_contents(os.path.normpath("events/"+name))
+        else:
+            return None
+
+    def get_include_contents(self, name):
+        if name.startswith("/"):
+            return self.get_contents(os.path.normpath("includes/"+name))
+        else:
+            return None
+
+    def get_event_names(self):
+        names = []
+        for name in self.cache.keys():
+            if name.startswith("events"):
+                names.append(name[6:])
+        return names
+
+    def get_include_names(self):
+        names = []
+        for name in self.cache.keys():
+            if name.startswith("includes"):
+                names.append(name[8:])
+        return names
+
+    def get_names(self):
+        return self.cache.keys()
+
+    def get_contents(self, tree_path):
+        return self.cache.get(tree_path)
+
+def get_user_hcron_tree_home(username, hostname):
+    """Hcron tree directory under user home.
+    """
+    return os.path.expanduser("~%s/.hcron/%s" % (username, hostname))
+
+def get_user_hcron_tree_filename(username, hostname):
+    """Hcron tree file under user home.
+    """
+    return os.path.normpath("%s/snapshot" % get_user_hcron_tree_home(username, hostname))
+
+def get_hcron_tree_home(username, hostname):
+    """Home of saved user hcron trees.
+    """
+    return os.path.normpath(HCRON_TREES_HOME)
+
+def get_hcron_tree_filename(username, hostname):
+    """Saved hcron tree file for user.
+    """
+    return os.path.normpath("%s/%s" % (get_hcron_tree_home(username, hostname), username))
+
+def create_user_hcron_tree_file(username, hostname, dst_path=None):
+    """Create an hcron tree file at dst_path with select members from
+    src_path.
+    """
+    if dst_path == None:
+        dst_path = get_user_hcron_tree_filename(username, hostname)
+
+    names = [ "includes", "events" ]
+    cwd = os.getcwd()
+    f = None
+
+    try:
+        if os.path.exists(dst_path):
+            os.remove(dst_path)
+
+        os.chdir(get_user_hcron_tree_home(username, hostname))
+        f = tarfile.open(dst_path, "w:gz")
+        for name in names:
+            f.add(name)
+        f.close()
+    except:
+        if f:
+            f.close()
+    os.chdir(cwd)
+
+    max_hcron_tree_snapshot_size = globals.config.get().get("max_hcron_tree_snapshot_size", CONFIG_MAX_HCRON_TREE_SNAPSHOT_SIZE)
+    if os.path.getsize(dst_path) > max_hcron_tree_snapshot_size:
+        raise Exception("snapshot file too big (>%s)" % max_hcron_tree_snapshot_size)
+
+def install_hcron_tree_file(username, hostname):
+    """Install/replace an hcron file for use by hcron-scheduler.
+    """
+    system_ht_home = get_hcron_tree_home(username, hostname)
+    src = get_user_hcron_tree_filename(username, hostname)
+    dst = get_hcron_tree_filename(username, hostname)
+
+    if not os.path.exists(system_ht_home):
+        os.makedirs(system_ht_home)
+
+    if os.path.exists(dst):
+        os.remove(dst)
+
+    max_hcron_tree_snapshot_size = globals.config.get().get("max_hcron_tree_snapshot_size", CONFIG_MAX_HCRON_TREE_SNAPSHOT_SIZE)
+    copyfile(src, dst, max_hcron_tree_snapshot_size)
+
