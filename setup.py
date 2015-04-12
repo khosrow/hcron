2ff558b090759741f49ec10d35dff7efdbfd85ec
Khosrow E <khosrow@khosrow.ca>
2015-04-11 21:01:05 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ import hcron
 setup(
     name='hcron',
     # version=hcron.__version__,
-    # author=hcron.__author__,
+    author='John Marshall',
     author_email='John.Marshall@ec.gc.ca',
     packages=['hcron'],
     url='https://bitbucket.org/hcron/hcron',
-    license='COPYRIGHT',
-    # description=hcron.__doc__.rstrip(),
+    license='GPLv2',
+    description='Hcron event scheduling tool',
     entry_points={
         'console_scripts': [
             'hcron-conv = hcron.hcron-conv:main',
